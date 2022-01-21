390a31d63030a41ddfc81017256ffecb422c5dcf
timokluser-dev <timo@timokluser.ch>
2022-01-21 10:13:33 +0100

    add RandomNumber.spec.js

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,19 @@
+import RandomNumber from "@/components/RandomNumber";
+import { mount } from "@vue/test-utils";
+
+describe("RandomNumber", async () => {
+  it("if button is clicked, randomNumber should be between 1 and 10", () => {
+    const wrapper = mount(RandomNumber);
+    wrapper.find("button").trigger("click");
+    expect(wrapper.vm.$data.randomNumber).toBeGreaterThanOrEqual(1);
+    expect(wrapper.vm.$data.randomNumber).toBeLessThanOrEqual(10);
+  });
+
+  it("if button is clicked, randomNumber should be between 200 and 300", async () => {
+    const wrapper = mount(RandomNumber);
+    await wrapper.setData({ min: 200, max: 300 });
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.vm.$data.randomNumber).toBeGreaterThanOrEqual(200);
+    expect(wrapper.vm.$data.randomNumber).toBeLessThanOrEqual(300);
+  });
+});
