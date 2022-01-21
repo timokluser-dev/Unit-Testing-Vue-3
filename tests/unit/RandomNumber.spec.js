6112ac58f314527b4a567bdacd5eebc46452616d
timokluser-dev <timo@timokluser.ch>
2022-01-21 13:17:05 +0100

    refactor: alternative way to set data

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -10,8 +10,18 @@ describe("RandomNumber", () => {
   });
 
   it("if button is clicked, randomNumber should be between 200 and 300", async () => {
-    const wrapper = mount(RandomNumber);
-    await wrapper.setData({ min: 200, max: 300 });
+    // const wrapper = mount(RandomNumber);
+    // await wrapper.setData({ min: 200, max: 300 });
+
+    const wrapper = mount(RandomNumber, {
+      data() {
+        return {
+          min: 200,
+          max: 300
+        };
+      }
+    });
+
     await wrapper.find("button").trigger("click");
     expect(wrapper.vm.$data.randomNumber).toBeGreaterThanOrEqual(200);
     expect(wrapper.vm.$data.randomNumber).toBeLessThanOrEqual(300);
