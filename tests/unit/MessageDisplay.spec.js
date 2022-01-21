beb9fe3e8b98f757c391d53fb4fe01f2946de87a
timokluser-dev <timo@timokluser.ch>
2022-01-21 10:59:45 +0100

    update MessageDisplay.spec.js

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -1,20 +1,36 @@
-import MessageDisplay from '@/components/MessageDisplay'
-import { mount } from '@vue/test-utils'
+import MessageDisplay from "@/components/MessageDisplay";
+import { mount } from "@vue/test-utils";
+import { getMessage } from "@/services/axios";
+import flushPromises from "flush-promises";
 
-describe('MessageDisplay', () => {
-  it('Calls getMessage and displays message', async () => {
-    // mock the API call
-    const wrapper = mount(MessageDisplay)
-    // wait for promise to resolve
-    // check that call happened once
-    // check that component displays message
-  })
+jest.mock("@/services/axios");
 
-  it('Displays an error when getMessage call fails', async () => {
-    // mock the failed API call
-    const wrapper = mount(MessageDisplay)
-    // wait for promise to resolve
-    // check that call happened once
-    // check that component displays error
-  })
-})
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("MessageDisplay", () => {
+  it("Calls getMessage and displays message", async () => {
+    const mockMessage = "Hello from the db!";
+    getMessage.mockResolvedValueOnce({ text: mockMessage });
+    const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+
+    const message = wrapper.find("[data-testid=\"message\"]").text();
+    expect(message).toEqual(mockMessage);
+  });
+
+  it("Displays an error when getMessage call fails", async () => {
+    const mockError = "Oops! Something went wrong.";
+    getMessage.mockRejectedValueOnce(mockError);
+    const wrapper = mount(MessageDisplay);
+
+    await flushPromises();
+    expect(getMessage).toHaveBeenCalledTimes(1);
+
+    const message = wrapper.find("[data-testid=\"message-error\"]").text();
+    expect(message).toEqual(mockError);
+  });
+});
